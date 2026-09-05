d35e968c975b49ba70fc9a7327d3507a4bcbd39b
Lucas Gass <lucas@bywatersolutions.com>
2026-09-05 01:10:59 +0000

    Add BSW, Koha, GNU stuff

--- Koha/Plugin/Com/ByWaterSolutions/FSRecordMetadata/Controller.pm
+++ Koha/Plugin/Com/ByWaterSolutions/FSRecordMetadata/Controller.pm
@@ -1,5 +1,22 @@
 package Koha::Plugin::Com::ByWaterSolutions::FSRecordMetadata::Controller;
 
+# Copyright ByWater Solutions 2026
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
 use Modern::Perl;
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::JSON qw(true false);
